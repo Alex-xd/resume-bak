89c2b575a2db4d3df8bab7fbf561a6bce9ac122f
Alex-xd <i@alexxd.com>
2017-02-19 20:08:30 +0800

    1

---
+++ static/app.js
@@ -0,0 +1,1 @@
+!function(t){function n(r){if(o[r])return o[r].exports;var e=o[r]={exports:{},id:r,loaded:!1};return t[r].call(e.exports,e,e.exports,n),e.loaded=!0,e.exports}var o={};return n.m=t,n.c=o,n.p="static/",n(0)}([function(t,n,o){t.exports=o(1)},function(t,n,o){o(2),o(4)},function(t,n){},,function(t,n){}]);
